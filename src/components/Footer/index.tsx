d9cef22da320c91897e911f25235b9e3291f78be
MastroCoder <fe03mastromauro@gmail.com>
2024-07-21 21:14:52 -0300

    fiz mailto

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,18 +6,28 @@ export default function Footer() {
     <footer className="relative min-w-full z-0 min-h-72 bg-[#1f271b] p-6 flex flex-row justify-between">
       <div className="flex flex-col space-y-4 w-2/5">
         <h4 className="text-[#fcfcfc] text-lg">Entre em contato!</h4>
-        <Textarea
-          placeholder="Mande uma proposta de emprego! Eu sei que você quer :3"
-          minRows={6}
-          className="min-w-xl"
-        />
-        <Button className="bg-[#46237a] text-[#fcfcfc]">
-          Enviar email
-        </Button>
+        <form action="mailto:fe03mastromauro@gmail.com" method="GET" className="space-y-4">
+          <Textarea
+            name="body"
+            placeholder="Mande uma proposta de emprego! Eu sei que você quer :3"
+            minRows={6}
+            className="min-w-xl"
+          />
+          <Button type="submit" className="w-full bg-[#46237a] text-[#fcfcfc]">
+            Enviar email
+          </Button>
+        </form>
       </div>
       <div className="flex flex-col space-y-4 w-auto justify-center">
-        <h4 className="text-[#fcfcfc] text-lg text-center">Este aqui sou eu!</h4>
-        <Image src="/pfp.jpeg" alt="Foto de Felipe Mastromauro Corrêa" width={190} className="align-center"/>
+        <h4 className="text-[#fcfcfc] text-lg text-center">
+          Este aqui sou eu!
+        </h4>
+        <Image
+          src="/pfp.jpeg"
+          alt="Foto de Felipe Mastromauro Corrêa"
+          width={190}
+          className="align-center"
+        />
       </div>
     </footer>
   );
